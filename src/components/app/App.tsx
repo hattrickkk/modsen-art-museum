60b980c5d324fbce8d10484ef2ed2f4a7fb9a78c
hattrick <dasha.khitrick@gmail.com>
2024-04-25 22:26:21 +0300

    init and set up project

---
+++ src/components/app/App.tsx
@@ -0,0 +1,10 @@
+import { GlobalStyles } from '@styles/global'
+import React from 'react'
+
+export const App = () => {
+    return (
+        <div>
+            <GlobalStyles />
+        </div>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        // filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html'),
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(ico|svg|png|gif|jpg|jpeg|avif)$/,
+                exclude: /fonts/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                loader: 'file-loader',
+            },
+            {
+                // test: /\.tsx?$/,
+                test: /\.m?(ts|js)x?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+
+    devServer: {
+        port: 3000,
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@styles': path.resolve(__dirname, 'src/styles'),
+        },
+    },
+}
