8001d990fca0150c4936e9e8543c4c910ac5d27e
hattrick <dasha.khitrick@gmail.com>
2024-04-28 17:10:39 +0300

    add favoritesSection component

---
+++ src/components/favoritesSection/FavoritesSection.tsx
@@ -0,0 +1,35 @@
+import CardsContainer from '@components/cardsContainer/CardsContainer'
+import { StyledContainer } from '@styles/styles'
+import Button from '@ui/button/Button'
+import SectionTitle from '@ui/sectionTitle/SectionTitle'
+import React from 'react'
+import { StyledFavoritesButtonWrapper, StyledFavoritesSection } from './styled'
+import { AppDispatch, AppState } from '@store/index'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeAllFromFavorites } from '@store/favorites/actions'
+
+const FavoritesSection = () => {
+    const favs = useSelector((state: AppState) => state.favs.list)
+    const dispatch = useDispatch<AppDispatch>()
+
+    return (
+        <StyledFavoritesSection>
+            <StyledContainer>
+                <SectionTitle title='Your favorites list' subtitle='Saved by you' />
+
+                <StyledFavoritesButtonWrapper>
+                    <Button
+                        value='Remove all from favorites'
+                        onClick={() => {
+                            dispatch(removeAllFromFavorites())
+                        }}
+                    />
+                </StyledFavoritesButtonWrapper>
+
+                <CardsContainer big={false} list={favs} />
+            </StyledContainer>
+        </StyledFavoritesSection>
+    )
+}
+
+export default FavoritesSection
---
+++ src/components/favoritesSection/styled.ts
@@ -0,0 +1,14 @@
+import { styled } from 'styled-components'
+
+export const StyledFavoritesSection = styled.section`
+    margin: 120px 0;
+
+    @media screen and (max-width: 980px) {
+        margin: 90px 0;
+    }
+`
+export const StyledFavoritesButtonWrapper = styled.div`
+    padding: 20px 0 40px 0;
+    display: flex;
+	justify-content: flex-end;
+`
