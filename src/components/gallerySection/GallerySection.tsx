0f1f5a259aa823513f12ded48c301013dc63d26a
hattrick <dasha.khitrick@gmail.com>
2024-04-28 23:27:32 +0300

    finish gallerySection (add pagination)

--- src/components/gallerySection/GallerySection.tsx
+++ src/components/gallerySection/GallerySection.tsx
@@ -8,27 +8,30 @@ import { AppDispatch, AppState } from '@store/index'
 import { loadPics } from '@store/pics/actions'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import Pagination from '@components/pagination/Pagination'
 
 const GallerySection = () => {
     const dispath = useDispatch<AppDispatch>()
     const pics = useSelector((state: AppState) => state.pics.list)
+    const totalPages = useSelector((state: AppState) => state.pics.totalPages)
 
     const { pageNumber } = useParams()
     const [currentPage, setCurrentPage] = useState<number>(pageNumber ? +pageNumber : 1)
 
     useEffect(() => {
-        dispath(loadPics(currentPage))
-    }, [])
-
-    useEffect(() => {
         setCurrentPage(pageNumber ? +pageNumber : 1)
     }, [pageNumber])
+
+    useEffect(() => {
+        dispath(loadPics(currentPage))
+    }, [currentPage])
 
     return (
         <StyledGallerySection>
             <StyledContainer>
                 <SectionTitle title='Our special gallery' subtitle='Topics for you' />
                 {!pics.length ? <Loader /> : <CardsContainer list={pics} big={true} />}
+                <Pagination currentPage={currentPage} pagesCount={totalPages} />
             </StyledContainer>
         </StyledGallerySection>
     )
