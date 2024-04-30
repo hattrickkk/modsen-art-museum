1fc3fba65a519200f086e735af066ac2d679ee65
hattrick <dasha.khitrick@gmail.com>
2024-04-30 10:25:14 +0300

    add react-error-boundary to dependencies

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,8 @@
 import OtherWorksSection from '@components/otherWorksSection/OtherWorksSection'
-import { StyledContainer, StyledPage } from '@styles/styles'
+import {StyledPage } from '@styles/styles'
 import React from 'react'
 import Title from '@ui/title/Title'
 import GallerySection from '@components/gallerySection/GallerySection'
-import Search from '@components/search/Search'
-import { useSelector } from 'react-redux'
-import { AppState } from '@store/index'
 import SearchBlock from '@components/searchBlock/SearchBlock'
 
 const HomePage = () => {
