d54c3418f35fb06c000c5d19dae9b3b24ffc7591
hattrick <dasha.khitrick@gmail.com>
2024-05-04 01:13:11 +0300

    feat: add constants for paths

---
+++ src/constants/paths.ts
@@ -0,0 +1,8 @@
+export const HOME_PAGE: string = '/'
+export const HOME_PAGE_WITH_ENDPOINT: string = '/:pageNumber'
+export const FAVORITES_PAGE: string = '/pictures/favorite'
+export const SINGLE_PAGE: string = '/picture/'
+export const SINGLE_PAGE_WITH_ENDPOINT: string = '/picture/:picId'
+export const SEARCH_PAGE: string = '/search/'
+export const SEARCH_PAGE_WITH_ENDPOINT: string = '/search/:searchPageNumber'
+export const NOT_FOUND_PAGE: string = '*'
