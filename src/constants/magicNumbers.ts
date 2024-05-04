ab882381650aab967eccabec2e2c40b79a77a2b0
hattrick <dasha.khitrick@gmail.com>
2024-05-04 14:51:25 +0300

    feat: add constants for magic numbers

---
+++ src/constants/magicNumbers.ts
@@ -0,0 +1,4 @@
+export const BIG_CARD_GAP: number = 30
+export const SMALL_CARD_GAP: number = 8
+export const COUNT_OF_AVAILABLE_SEARCH_RESULTS_PAGES: number = 111
+export const MAX_LENGTH_FOR_EXHIBITION_HISTORY_FIELD: number = 700
