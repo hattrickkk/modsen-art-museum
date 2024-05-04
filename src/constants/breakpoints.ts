c47b7c0d4a2d5dad841b7118d4eeb7c08791b06b
hattrick <dasha.khitrick@gmail.com>
2024-05-04 17:08:52 +0300

    feat: add breakpoints constants

---
+++ src/constants/breakpoints.ts
@@ -0,0 +1,15 @@
+export const MEDIA_QUERY_PHONE_SMALL: string = 'screen and (max-width: 430px)'
+export const MEDIA_QUERY_PHONE_NORMAL: string = 'screen and (max-width: 500px)'
+
+export const MEDIA_QUERY_TABLET_EXTRA_SMALL: string = 'screen and (max-width: 615px)'
+export const MEDIA_QUERY_TABLET_SMALL: string = 'screen and (max-width: 710px)'
+export const MEDIA_QUERY_TABLET_NORMAL: string = 'screen and (max-width: 780px)'
+export const MEDIA_QUERY_TABLET_LARGE: string = 'screen and (max-width: 910px)'
+export const MEDIA_QUERY_TABLET_EXTRA_LARGE: string = 'screen and (max-width: 980px)'
+
+export const MEDIA_QUERY_LAPTOP: string = 'screen and (max-width: 1045px)'
+export const MEDIA_QUERY_DESKTOP_SMALL: string = 'screen and (max-width: 1212px)'
+export const MEDIA_QUERY_DESKTOP: string = 'screen and (max-width: 1280px)'
+
+export const MEDIA_QUERY_MEDIUM_RANGE: string = 'screen and (max-width: 862px) and (min-width: 780px)'
+export const MEDIA_QUERY_LARGE_RANGE: string = 'screen and (max-width: 1212px) and (min-width: 780px)'
