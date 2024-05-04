fd7830de2ed53f21cfa0fcd2bb99ce7c89c3f2b2
hattrick <dasha.khitrick@gmail.com>
2024-05-04 18:43:07 +0300

    feat: add errorBoundary class component

---
+++ src/components/errorBoundary/index.tsx
@@ -0,0 +1,51 @@
+import { StyledPage, StyledContainer } from '@styles/styles'
+import Button from '@ui/button/Button'
+import React, { Component, ReactNode } from 'react'
+import { StyledErrorFalback, StyledErrorFalbackText, StyledErrorFalbackTitle } from './styled'
+import { Link } from 'react-router-dom'
+import { HOME_PAGE } from '@constants/paths'
+
+type PropsType = {
+    children?: ReactNode
+}
+
+type StateType = {
+    hasError: boolean
+    errorName: string
+    errorMessage: string
+}
+
+const initValue: StateType = {
+    hasError: false,
+    errorName: '',
+    errorMessage: '',
+}
+
+class ErrorBoundary extends Component<PropsType, StateType> {
+    public state: StateType = initValue
+
+    public static getDerivedStateFromError(error: Error): StateType {
+        return { hasError: true, errorMessage: error.message, errorName: error.name }
+    }
+
+    public render() {
+        if (this.state.hasError) {
+            return (
+                <StyledPage>
+                    <StyledContainer>
+                        <StyledErrorFalback>
+                            <StyledErrorFalbackTitle>Oops, something went wrong</StyledErrorFalbackTitle>
+                            <StyledErrorFalbackText>{`${this.state.errorName}: ${this.state.errorMessage}`}</StyledErrorFalbackText>
+                            <Link to={HOME_PAGE} onClick={() => this.setState(initValue)}>
+                                <Button onClick={() => {}} value='Click to reload page' />
+                            </Link>
+                        </StyledErrorFalback>
+                    </StyledContainer>
+                </StyledPage>
+            )
+        }
+
+        return this.props.children
+    }
+}
+export default ErrorBoundary
