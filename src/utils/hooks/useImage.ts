04bd0f13221e47159254734e3ffecfc6480f2c37
hattrick <dasha.khitrick@gmail.com>
2024-05-05 00:46:37 +0300

    fix: add argument for dependencies in useImage hook

--- src/utils/hooks/useImage.ts
+++ src/utils/hooks/useImage.ts
@@ -1,11 +1,15 @@
+import { SinglePicType } from '@customTypes/singlePicture'
 import { useState, useEffect } from 'react'
 
-export const useImage = (path: string): string | null => {
+export const useImage = (path: string, dependencies?: SinglePicType): string | null => {
     const [imageURL, setImageURL] = useState<null | string>(null)
 
-    useEffect(() => {
-        fetch(path).then(res => setImageURL(res.status === 200 ? path : null))
-    }, [])
+    useEffect(
+        () => {
+            fetch(path).then(res => setImageURL(res.status === 200 ? path : null))
+        },
+        dependencies ? [dependencies] : []
+    )
 
     return imageURL
 }
