350e4bff64d42e90f08c29fa953d71d7db2cdaf6
hattrick <dasha.khitrick@gmail.com>
2024-05-04 17:49:54 +0300

    fix: fix loader componet using custom styles without other libraries
    BREAKING CHANGES: "react-spinners" are no longer needed

--- src/ui/loader/Loader.tsx
+++ src/ui/loader/Loader.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import { StyledLoader } from './styled'
-import { ClipLoader } from 'react-spinners'
-import * as palette from '@constants/stylesConstants'
+import { StyledLoader, StyledLoaderWrapper } from './styled'
 import { Flex } from '@styles/flexStyles'
 
 const Loader = () => {
     return (
-        <StyledLoader>
+        <StyledLoaderWrapper>
             <Flex $justifycontent='center'>
-                <ClipLoader color={palette.ORANGE_COLOR} size={150} />
+                <StyledLoader />
             </Flex>
-        </StyledLoader>
+        </StyledLoaderWrapper>
     )
 }
 
--- src/ui/loader/styled.ts
+++ src/ui/loader/styled.ts
@@ -1,5 +1,26 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+import * as palette from '@constants/stylesConstants'
 
-export const StyledLoader = styled.div`
+const loaderAnimation = keyframes`
+    0% {
+        transform: scale(1) rotate(0);
+    }
+    50%{
+        transform: scale(1.3) rotate(180deg);
+    }
+    100%{
+        transform: scale(1) rotate(360deg);
+    }
+`
+
+export const StyledLoaderWrapper = styled.div`
     padding: 65px 0;
+`
+export const StyledLoader = styled.div`
+    height: 120px;
+    width: 120px;
+    border: 2px solid ${palette.ORANGE_COLOR};
+    border-radius: 50%;
+    border-left-color: transparent;
+    animation: ${loaderAnimation} 0.8s linear infinite;
 `
