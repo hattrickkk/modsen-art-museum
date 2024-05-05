228783683acf17f123420fee916fd444c4aaa6d5
hattrick <dasha.khitrick@gmail.com>
2024-05-06 01:00:37 +0300

    fix(otherWorksSection): add request error handling

--- src/components/otherWorksSection/index.tsx
+++ src/components/otherWorksSection/index.tsx
@@ -7,21 +7,30 @@ import Loader from '@ui/loader/Loader'
 import { getPics } from '@utils/api/getPics'
 import { PicType } from '@customTypes/picture'
 import { getPicItem } from '@utils/getPicItem'
+import NoResultsFound from '@ui/noResultsfound'
 
 const OtherWorksSection = () => {
     const [otherPics, setOtherPics] = useState<PicType[]>([])
+    const [hasErrors, setHasErrors] = useState<boolean>(false)
 
     useEffect(() => {
         getPics(9)
             .then(res => getPicItem(res))
             .then(res => setOtherPics(res))
+            .catch(() => setHasErrors(true))
     }, [])
 
     return (
         <StyledOtherWorksSection>
             <StyledContainer>
                 <SectionTitle title='Other works for you' subtitle='Here some more' />
-                {!otherPics.length ? <Loader /> : <CardsContainer list={otherPics} big={false} />}
+                {hasErrors ? (
+                    <NoResultsFound />
+                ) : !otherPics.length ? (
+                    <Loader />
+                ) : (
+                    <CardsContainer list={otherPics} big={false} />
+                )}
             </StyledContainer>
         </StyledOtherWorksSection>
     )
