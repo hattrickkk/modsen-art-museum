cb378aa36cbd5fbb7c77c1267d4c63477b4e176f
hattrick <dasha.khitrick@gmail.com>
2024-05-06 18:54:42 +0300

    fix(api): add date_start and date_end params in query in getSearchResults function

--- src/utils/api/getSearchResults.ts
+++ src/utils/api/getSearchResults.ts
@@ -1,6 +1,6 @@
-import { ResponseType } from '@customTypes/picture'
+import { SearchResponseType } from '@customTypes/search'
 
-export const getSearchResults = async (search: string, page: number = 1): Promise<ResponseType> => {
+export const getSearchResults = async (search: string, page: number = 1): Promise<SearchResponseType> => {
     const response = await fetch(
         `https://api.artic.edu/api/v1/artworks/search?fields=id,title,image_id,artist_display,is_public_domain,date_start,date_end&limit=9&page=${page}&q=${search}`
     )
