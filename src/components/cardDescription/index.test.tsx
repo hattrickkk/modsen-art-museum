6ae35049977d190d093c2ce0fa29016102790005
hattrick <dasha.khitrick@gmail.com>
2024-05-07 14:55:44 +0300

    test: fix tests for cardDescription component

--- src/components/cardDescription/index.test.tsx
+++ src/components/cardDescription/index.test.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import { PicType } from '@customTypes/picture'
 import CardDescription from '.'
 import { Provider } from 'react-redux'
 import store from '@store/index'
-
-const item: PicType = {
-    id: 105293,
-    title: 'Chest of Drawers',
-    author: 'Artist unknown',
-    isPublic: true,
-    image: 'https://www.artic.edu/iiif/2/8cb72d4a-0be3-4a3d-63bc-b93bd6694fe6/full/843,/0/default.jpg',
-}
+import { MOCK_ITEM } from '@constants/mockData'
 
 describe('cardsDescription components', () => {
     test('cardsDescription renders with mockData', () => {
         render(
             <Provider store={store}>
-                <CardDescription item={item} />
+                <CardDescription item={MOCK_ITEM} />
             </Provider>
         )
 
-        expect(screen.getByText(item.title)).toBeInTheDocument()
+        expect(screen.getByText(MOCK_ITEM.title)).toBeInTheDocument()
+        expect(screen.getByText(MOCK_ITEM.author as string)).toBeInTheDocument()
+        expect(screen.getByText('Public')).toBeInTheDocument()
     })
 })
